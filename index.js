7c0c81ee9932df624acc9970cb9bb492da0902a7
Ngun Lin Uk <Ngunuk01@gmail.com>
2018-05-17 15:14:17 -0400

    Add the flick to an unordered list

--- index.js
+++ index.js
@@ -1,29 +1,51 @@
 const app = {
-  init: function(formSelector) {
+  init(selectors) {
     this.max = 0
+    this.list = document.querySelector(selectors.listSelector)
 
     document
-      .querySelector(formSelector)
+      .querySelector(selectors.formSelector)
       .addEventListener('submit', ev => {
         ev.preventDefault()
         this.handleSubmit(ev)
       })
   },
 
-  handleSubmit: function(ev) {
+  renderListItem(flick) {
+    const item = document.createElement('li')
+    item.textContent = flick.name
+    return item
+  },
+
+  handleSubmit(ev) {
     const f = ev.target
     const flick = {
       id: ++this.max,
       name: f.flickName.value,
     }
-    console.log(flick)
+
+    const item = this.renderListItem(flick)
+    this.list.appendChild(item)
+
     f.reset()
   },
 }
 
-app.init('#flickForm')
-
+app.init({
+  formSelector: '#flickForm',
+  listSelector: '#flickList',
+})
   /*
   id: NaN  ==== not a number
   => --when you use this, the outcome results in what we think it will be
+
+  To check the list ==== app.list
+  now we got a list; append these
+  --renderListItem:function(){
+    return item
+  }
+
+  --renderListItem ==append it to == this.list.appendChild(item)
+    
+
   
